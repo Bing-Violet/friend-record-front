fbc7a4cd14beb90614c884a877a89a42cb91f18c
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-05-08 20:10:46 +1000

    add patch and delete functions for event.

--- pages/friendDetails/[slug].js
+++ pages/friendDetails/[slug].js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef, forwardRef, useContext } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import {
@@ -13,6 +13,11 @@ import {
   Avatar,
   Text,
   Box,
+  Stack,
+  FocusLock,
+  FormControl,
+  FormLabel,
+  Input,
 } from "@chakra-ui/react";
 import {
   Menu,
@@ -23,16 +28,42 @@ import {
   MenuGroup,
   MenuOptionGroup,
   MenuDivider,
-  IconButton
+  IconButton,
 } from "@chakra-ui/react";
+
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+  Portal
+} from "@chakra-ui/react";
+
+import {
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from "@chakra-ui/react";
+
+import { useDisclosure } from "@chakra-ui/react";
+
 import EventCreate from "@/components/eventCreate";
 import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { AiOutlineEdit } from "react-icons/ai";
 import { FiDelete } from "react-icons/fi";
+import AppContext from "@/components/globalContext";
 
 export default function FriendDetail({ user }) {
   const router = useRouter();
+  const context = useContext(AppContext);
   const [friend, setFriend] = useState("");
   const [events, setEvents] = useState("");
   const [mounted, setMounted] = useState(false);
@@ -45,9 +76,9 @@ export default function FriendDetail({ user }) {
       })
         .then((res) => {
           setFriend(res.data);
-          setEvents(res.data.event)
+          setEvents(res.data.event);
           console.log("res_slug", res.data, events);
-          setMounted(true)
+          setMounted(true);
         })
         .catch((e) => {});
     } else if (!slug) {
@@ -83,7 +114,6 @@ export default function FriendDetail({ user }) {
                   <Text>Name : {friend.name}</Text>
                   <Text>Sum : ${friend.sum}</Text>
                   <Text>Last_log : {dateConvert(friend.last_log)}</Text>
-                  {console.log("IN_SLUG", events)}
                 </VStack>
               </Flex>
             </CardBody>
@@ -102,33 +132,208 @@ export default function FriendDetail({ user }) {
         {events.length ? (
           <>
             {events.map((e, index) => (
-             <Card position={'relative'} key={index} mb={"1rem"} minW={"100%"}>
-              <SettingMenu/>
-             <CardBody>
-               <Flex alignItems={"center"}>
-                 <VStack align="stretch">
-                   <Text>Event-Name : {e.name}</Text>
-                   <Text>Money : ${e.money}</Text>
-                   <Text>Created : {dateConvert(e.created_on)}</Text>
-                 </VStack>
-               </Flex>
-             </CardBody>
-           </Card>
+              <Card position={"relative"} key={index} mb={"1rem"} minW={"100%"}>
+                <EditPopover eventName={e.name} money={e.money} id={e.id} />
+                <CardBody>
+                  <Flex alignItems={"center"}>
+                    <VStack align="stretch">
+                      <Text>Event-Name : {e.name}</Text>
+                      <Text>Money : ${e.money}</Text>
+                      <Text>Created : {dateConvert(e.created_on)}</Text>
+                    </VStack>
+                  </Flex>
+                </CardBody>
+                <DeletePopover id={e.id}/>
+              </Card>
             ))}
-            <EventCreate slug={router.query.slug} friend={friend} events={events} setEvents={setEvents}/>
+            <EventCreate
+              slug={router.query.slug}
+              friend={friend}
+              events={events}
+              setEvents={setEvents}
+            />
           </>
         ) : (
-            <Flex flexDirection={"column"} alignItems={"center"}>
+          <Flex flexDirection={"column"} alignItems={"center"}>
             <Text>No Event To Show</Text>
-            <EventCreate slug={router.query.slug} friend={friend} events={events} setEvents={setEvents}/>
-            </Flex>
+            <EventCreate
+              slug={router.query.slug}
+              friend={friend}
+              events={events}
+              setEvents={setEvents}
+            />
+          </Flex>
         )}
+      </Box>
+    );
+  }
+
+  function DeletePopover({id}) {
+    function deleteEvent() {
+      axios({
+        method: "delete",
+        url: `/api/event/event-detail/${id}`,
+      })
+        .then((res) => {
+           //need to change character data
+          const newEvents = events.filter(e => e.id !== id);
+          events.forEach((e) => {
+            if(e.id===id) {
+              friend.sum -= e.money
+            }
+          })
+          setEvents([...newEvents]);
+          onCancel();
+        })
+        .catch((e) => {
+          console.log("error", e);
+        });
+    }
+    return (
+      <>
+        <Popover>
+          <PopoverTrigger>
+            <Button background={'red.700'} color={'red.100'}>DELETE</Button>
+          </PopoverTrigger>
+          <Portal>
+            <PopoverContent background={'pink.400'}>
+              <PopoverArrow />
+              <PopoverHeader>Confirmation</PopoverHeader>
+              <PopoverCloseButton />
+              <PopoverBody>
+                are you sure? 
+              </PopoverBody>
+              <PopoverFooter textAlign={'center'}>
+              <Button onClick={deleteEvent} colorScheme="blue">Delete</Button>
+              </PopoverFooter>
+            </PopoverContent>
+          </Portal>
+        </Popover>
+      </>
+    );
+  }
+
+  function EditPopover({ eventName, money, id }) {
+    const { onOpen, onClose, isOpen } = useDisclosure();
+    const firstFieldRef = useRef(null);
+    const TextInput = forwardRef((props, ref) => {
+      return (
+        <FormControl>
+          <FormLabel htmlFor={props.id}>{props.label}</FormLabel>
+          <Input ref={ref} id={props.id} {...props} />
+        </FormControl>
+      );
+    });
+
+    const Form = ({ firstFieldRef, onCancel }) => {
+      const [editedEventName, setEditedEventName] = useState("");
+      const [editedMoney, setEditedMoney] = useState("default");
+      const [isDisabled, setIsDisabled] = useState(true);
+
+      const eventHandleChange = (event) => {
+        setEditedEventName(event.target.value), setIsDisabled(false);
+      };
+      const moneyHandleChange = (event) => {
+        setEditedMoney(event), setIsDisabled(false);
+      };
+      function saveFunc() {
+        console.log(eventName, editedEventName, editedMoney, typeof money);
+        axios({
+          method: "patch",
+          url: `/api/event/event-detail/${id}`,
+          data: {
+            name: !editedEventName ? eventName : editedEventName,
+            money: editedMoney !== "default" ? editedMoney : money,
+          },
+        })
+          .then((res) => {
+            const  updatedName = !editedEventName ? eventName : editedEventName
+            if(editedMoney !== "default") {
+              friend.sum += editedMoney - money
+            }
+            
+            const newEvents = events.map((e) => {
+              if (e.id === id) {
+                (e.name = updatedName),
+                  (e.money = editedMoney !== "default" ? editedMoney : money);
+                return e;
+              } else {
+                return e;
+              }
+            });
+            setEvents([...newEvents]);
+            onCancel();
+          })
+          .catch((e) => {
+            console.log("error", e);
+            setApiError(true);
+          });
+      }
+      return (
+        <Stack spacing={4}>
+          <TextInput
+            label="Event name"
+            id="first-name"
+            ref={firstFieldRef}
+            defaultValue={eventName}
+            onChange={eventHandleChange}
+          />
+          <Box>Money</Box>
+          <NumberInput
+            size="md"
+            maxW={40}
+            onChange={moneyHandleChange}
+            defaultValue={money}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+          <ButtonGroup display="flex" justifyContent="flex-end">
+            <Button variant="outline" onClick={onCancel}>
+              Cancel
+            </Button>
+            <Button
+              isDisabled={isDisabled}
+              onClick={saveFunc}
+              colorScheme="teal"
+            >
+              Save
+            </Button>
+          </ButtonGroup>
+        </Stack>
+      );
+    };
+
+    return (
+      <Box position={"absolute"} right={"0"}>
+        <Popover
+          isOpen={isOpen}
+          initialFocusRef={firstFieldRef}
+          onOpen={onOpen}
+          onClose={onClose}
+          placement="right"
+          closeOnBlur={false}
+        >
+          <PopoverTrigger>
+            <IconButton size="sm" icon={<AiOutlineEdit />} />
+          </PopoverTrigger>
+          <PopoverContent p={5}>
+            <FocusLock returnFocus persistentFocus={false}>
+              <PopoverArrow />
+              <PopoverCloseButton />
+              <Form firstFieldRef={firstFieldRef} onCancel={onClose} />
+            </FocusLock>
+          </PopoverContent>
+        </Popover>
       </Box>
     );
   }
   function SettingMenu() {
     return (
-      <Box position={"absolute"} right={'0'}>
+      <Box position={"absolute"} right={"0"}>
         <Menu>
           <MenuButton
             as={IconButton}
@@ -137,12 +342,8 @@ export default function FriendDetail({ user }) {
             variant="ghost"
           />
           <MenuList>
-            <MenuItem icon={<AiOutlineEdit />}>
-             Edit
-            </MenuItem>
-            <MenuItem icon={<FiDelete />}>
-              Delete
-            </MenuItem>
+            <MenuItem icon={<AiOutlineEdit />}>Edit</MenuItem>
+            <MenuItem icon={<FiDelete />}>Delete</MenuItem>
           </MenuList>
         </Menu>
       </Box>
@@ -150,14 +351,16 @@ export default function FriendDetail({ user }) {
   }
   return (
     <>
-    {mounted ? (
+      {mounted ? (
         <Center width={"100%"}>
-            <Flex w={"600px"} alignItems={"center"} flexDirection={"column"}>
-                <FriendInfo />
-                <EventList/>
-            </Flex>
-      </Center>
-    ):(<></>)}
+          <Flex w={"600px"} alignItems={"center"} flexDirection={"column"}>
+            <FriendInfo />
+            <EventList />
+          </Flex>
+        </Center>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
